9525ebcd4d741cf4a27efb8b49ce0d4eff0aa72f
Mohammad Shafizadeh <pouyash82@192.168.1.53>
2026-04-11 14:29:36 +0300

    Fix API URL

--- src/api/index.js
+++ src/api/index.js
@@ -1,27 +1,15 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
 })
 
-// Auto-attach token to every request
-API.interceptors.request.use(config => {
+API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
-  if (token) config.headers.Authorization = `Bearer ${token}`
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
   return config
 })
-
-// Auto-logout on 401
-API.interceptors.response.use(
-  res => res,
-  err => {
-    if (err.response?.status === 401) {
-      localStorage.removeItem('token')
-      localStorage.removeItem('user')
-      window.location.href = '/login'
-    }
-    return Promise.reject(err)
-  }
-)
 
 export default API
